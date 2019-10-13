d4677da2b8a64eb3e3b97cb0ac3e23441debc97e
Brian <juniortoad1738@gmail.com>
2019-10-13 14:41:34 -0400

    Navigation View Fix

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -1,25 +1,35 @@
 import React, {Fragment, Component} from 'react';
-import {SafeAreaView, StyleSheet, NativeModules} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  NativeModules,
+  Button
+} from 'react-native';
 //import all the components we are going to use.
-
 
 
 export default class Directions extends Component {
 
-render(){
-  var navDemo = NativeModules.NavDemo;
-  navDemo.renderNaviDemo(
-    (originLat = 41.3282668),
-    (originLon = -72.9248731),
-    (originName = 'Current Location'),
-    (destinationLat = 41.311587),
-    (destinationLon = -72.929541),
-    (destinationName = "Toad's Place"),
-  );
+  render(){
 
-  return(
-    <SafeAreaView></SafeAreaView>
-  )
-}
 
+    return(
+      <SafeAreaView>
+        <Button
+          onPress={() => {
+            var navDemo = NativeModules.NavDemo;
+            navDemo.renderNaviDemo(
+              (originLat = 41.3282668),
+              (originLon = -72.9248731),
+              (originName = 'Current Location'),
+              (destinationLat = 41.311587),
+              (destinationLon = -72.929541),
+              (destinationName = "Toad's Place"),
+            );
+          }}
+          title="Press Me"
+        />
+      </SafeAreaView>
+    )
+  }
 }
