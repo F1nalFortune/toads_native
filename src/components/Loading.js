cd63a7dd257a530532898af896a2a80f3630d0cb
Brian <juniortoad1738@gmail.com>
2020-03-19 16:50:01 -0400

    moved homescreen to events page

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,7 @@ import firebase from 'react-native-firebase'
 export default class Loading extends Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
-      this.props.navigation.navigate(user ? 'Venue' : 'SignUp')
+      this.props.navigation.navigate(user ? 'Music' : 'SignUp')
     })
   }
   render() {
