ed5b9c4570d09a17fb91ae99637b919ce0ea9d4c
Brian <juniortoad1738@gmail.com>
2020-04-02 15:52:32 -0400

    fixed dependency issues (airmaps)

--- android/app/src/main/java/com/navdemo/MainApplication.java
+++ android/app/src/main/java/com/navdemo/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
